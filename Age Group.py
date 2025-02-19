de3c7622fd81f4f5d2f44dce8e95241ee8ee9d86
GeneralGarfield <149305104+GeneralGarfield@users.noreply.github.com>
2025-02-19 11:06:22 -0600

    Add files via upload
    Its been a while

---
+++ Age Group.py
@@ -0,0 +1,34 @@
+# Program Age Classifier
+# Description: Determines the age and organizes it.
+#   
+#
+# Author: <Ricardo Montoya-Adame> 
+# Date: <19/2/2025>
+# Revised: 
+#   <19/2/2025>
+
+
+#Prints opening Statement
+
+print("Organizes the age and places it in categories. By Ricardo Montoya-Adame")
+
+# This Determines the age and organizes based on the age group
+def persons_age():
+    person = int(input("What is the age of the person?: "))
+    if person <= 1:
+        print("they are an infant")
+    elif person in range(2,14):
+        print("they are a child")
+    elif person in range(14,20):
+        print("they are a teenager")
+    elif person in range(20,61):
+        print("They are an adult")
+    elif person in range(61,110):
+        print("You are a senior")
+    elif person > 111:
+        print("You're really old. Congrats.")
+
+# Runs the Program
+persons_age()
+
+#End of Program
